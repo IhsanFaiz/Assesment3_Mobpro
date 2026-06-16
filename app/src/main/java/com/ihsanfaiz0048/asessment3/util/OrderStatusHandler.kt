59c333d1235c110e8468f8c3cf3da6c6877245ec
Ihsan Faiz <ihsanfaiz200@gmail.com>
2026-06-16 16:46:06 +0700

    configure package

--- app/src/main/java/com/ihsanfaiz0048/asessment3/util/OrderStatusHandler.kt
+++ app/src/main/java/com/ihsanfaiz0048/asessment3/util/OrderStatusHandler.kt
@@ -1,7 +1,6 @@
 package com.ihsanfaiz0048.asessment3.util
 
 import com.ihsanfaiz0048.asessment3.model.OrderWithMenu
-import kotlin.compareTo
 
 fun OrderWithMenu.getStatus(currentTime: Long): OrderStatus{
     val now = System.currentTimeMillis()
@@ -10,10 +9,10 @@ fun OrderWithMenu.getStatus(currentTime: Long): OrderStatus{
 
     return when {
 
-        elapsed compareTo 30_000 ->
+        elapsed < 30_000 ->
             OrderStatus.PENDING
 
-        elapsed compareTo 60_000 ->
+        elapsed < 60_000 ->
             OrderStatus.PROCESSING
 
         else ->
